3a3324fcf635e51dc45306234d7406e74826f0d1
Pierre BOURDU <pierre.bourdu@gmail.com>
2016-03-12 09:40:19 +0100

    ajout des models ecriture et ligne + du basemodel

--- QreaCompta.js
+++ QreaCompta.js
@@ -1,29 +1,65 @@
-var QreaCompta = (function(){
+var QreaCompta = (function() {
 
-    var self = {};
+  var self = {};
 
-    /*
-    self.init = function(){
+  /*
+  self.init = function(){
 
-    };
-    */
+  };
+  */
 
-    return self;
+  return self;
 
 })();
 
-QreaCompta.models = (function(){
+QreaCompta.models = (function() {
 
-    var self = {};
+  var self = {};
 
-   self.Journal = function(params){
+  var BaseModel = function(params){
 
+    this.prototype.ini = function(){
+      console.log('ini du model');
+    };
 
-     this.code = params.code || null;
-     this.libelle = params.libelle || null;
+  };
 
-   };
+  self.Journal = function(params) {
 
-    return self;
+    this.prototype = new BaseModel();
+
+    this.journalCode = params.journalCode || null;
+    this.journalLibelle = params.journalLibelle || null;
+
+    this.ecritures = params.ecritures || [];
+
+    this.ini();
+
+  };
+
+  self.Ecriture = function(params){
+
+    this.dateLet = params.dateLet || null;
+    this.ecritureDate = params.ecritureDate || null;
+    this.ecritureLet = params.ecritureLet || null;
+    this.ecritureLib = params.ecritureLib || null;
+    this.pieceDate = params.pieceDate || null;
+    this.pieceRef = params.pieceRef || null;
+    this.validDate = params.validDate || null;
+
+    this.lignes = params.lignes || [];
+
+  };
+
+  self.Ligne = function(params){
+
+    this.compteLib = params.compteLib || null;
+    this.compteNum = params.compteNum || null;
+    this.credit = params.credit || null;
+    this.debit = params.devit || null;
+
+  };
+
+  return self;
 
 })();
