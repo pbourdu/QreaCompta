ce6d6594ea691358a9e707ff51bbf52e00615f76
Pierre BOURDU <pierre.bourdu@gmail.com>
2016-03-16 18:15:45 +0100

    Update Models.ts
    Ajout d'une class Compte

--- Models/Models.ts
+++ Models/Models.ts
@@ -13,6 +13,25 @@ module QreaCompta {
       }
     };
 
+    export class Compte extends BaseModel {
+      
+      constructor(params: any){
+        
+        // constructor parent
+        super(params);
+        this.compteLib = params.compteLib || null;
+        this.compteNum = params.compteNum || null;
+          
+      }
+      
+      // le libellé du compte
+      compteLib: string;
+      
+      // c'est l'identifiant unique du compte
+      compteNum: string;
+      
+    }
+    
     export class Journal extends BaseModel {
 
       constructor(params: any) {
